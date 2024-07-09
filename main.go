ed25c88d24782f76e77eb89986102611f8be8fdd
hohotang <brightrobber@gmail.com>
2024-07-09 20:25:03 +0800

    禁用快速編輯模式

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"log"
 	"os"
 	"tgbot/app"
+
+	"golang.org/x/sys/windows"
 )
 
 var version string = "0"
+
+// Constants for console modes
+const (
+	ENABLE_EXTENDED_FLAGS  = 0x0080
+	ENABLE_QUICK_EDIT_MODE = 0x0040
+)
 
 func main() {
 	tgApp := app.NewApp(version)
@@ -14,6 +22,28 @@ func main() {
 }
 
 func init() {
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	disableQuickEditMode()
+}
+
+// disableQuickEditMode disables the Quick Edit mode in the console.
+func disableQuickEditMode() {
+	hStdin := windows.Handle(os.Stdin.Fd())
+
+	// Get current console mode
+	var mode uint32
+	err := windows.GetConsoleMode(hStdin, &mode)
+	if err != nil {
+		log.Printf("GetConsoleMode failed: %v\n", err)
+		return
+	}
+
+	// Disable Quick Edit mode
+	mode &^= ENABLE_QUICK_EDIT_MODE
+	mode |= ENABLE_EXTENDED_FLAGS
+
+	// Set new console mode
+	err = windows.SetConsoleMode(hStdin, mode)
+	if err != nil {
+		log.Printf("SetConsoleMode failed: %v\n", err)
+	}
 }
